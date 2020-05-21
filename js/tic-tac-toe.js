174b163e0b475e9768d3efa71349c958bf401c9b
AggieWB <usmaggie23@gmail.com>
2020-05-21 15:37:42 -0700

    Move anonymous function to global handleClick function
    Add removeEventListener() to addNought() and addCross()

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,9 +1,6 @@
 function addElementListener(elements){
     for(var i = 1; i < elements.length; i++){
-        //event is the object that represents the event fired
-        //target is an object that represents the element that fire the event
-        //cannot use elements[i].id since when the event is fired, i will always be outside of the elements NodeList indexes
-        elements[i].addEventListener('click', function(event){addNought(event.target.id);});
+        elements[i].addEventListener('click', handleClick);
     }
 }
 
@@ -12,6 +9,7 @@ function addNought(selectedElementId){
     nought.setAttribute('src', 'media/nought.png');
     var element = document.getElementById(selectedElementId);
     element.appendChild(nought);
+    element.removeEventListener('click', handleClick);
 }
 
 function addCross(selectedElementId){
@@ -19,6 +17,14 @@ function addCross(selectedElementId){
     cross.setAttribute('src', 'media/cross.png');
     var element = document.getElementById(selectedElementId);
     element.appendChild(cross);
+    element.removeEventListener('click', handleClick);
+}
+
+function handleClick(event){
+    //event is the object that represents the event fired
+    //target is an object that represents the element that fire the event
+    //cannot use elements[i].id since when the event is fired, i will always be outside of the elements NodeList indexes
+    addNought(event.target.id);
 }
 
 addElementListener(document.getElementsByTagName('div'));
