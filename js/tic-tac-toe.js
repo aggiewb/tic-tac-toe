7cafccdc916f73687ff00b49ae61439aef9dd4e6
AggieWB <usmaggie23@gmail.com>
2020-05-21 17:43:33 -0700

    Add else if block for selectionClick eventListener
    Change addCross() and addNought() to only pass in html element retrieved by eventListner
    Add function to handle selection click event

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,30 +1,38 @@
 function addElementListener(elements){
-    for(var i = 1; i < elements.length; i++){
-        elements[i].addEventListener('click', handleClick);
+    for(var i = 0; i < elements.length; i++){
+        if(elements[i].id.includes('box')){
+            elements[i].addEventListener('click', handleBoardClick);
+        } else if(elements[i].id.includes('cross') || elements[i].id.includes('nought')){
+            elements[i].addEventListener('click', handleSelectionClick);
+        }
     }
 }
 
-function addNought(selectedElementId){
+function addNought(element){
     var nought = document.createElement('img');
     nought.setAttribute('src', 'media/nought.png');
-    var element = document.getElementById(selectedElementId);
     element.appendChild(nought);
     element.removeEventListener('click', handleClick);
 }
 
-function addCross(selectedElementId){
+function addCross(element){
     var cross = document.createElement('img');
     cross.setAttribute('src', 'media/cross.png');
-    var element = document.getElementById(selectedElementId);
     element.appendChild(cross);
     element.removeEventListener('click', handleClick);
 }
 
-function handleClick(event){
+function handleBoardClick(event){
     //event is the object that represents the event fired
-    //target is an object that represents the element that fire the event
-    //cannot use elements[i].id since when the event is fired, i will always be outside of the elements NodeList indexes
-    addNought(event.target.id);
+    //target is an object that represents the element that fired the event
+    //cannot use elements[i] since when the event is fired, i will always be outside of the elements' NodeList indexes
+    addNought(event.target);
+}
+
+function handleSelectionClick(event){
+    var userMarker = event.target.id;
+    var selectionDiv = event.target.parentNode.parentNode;
+    selectionDiv.innerHTML = "";
 }
 
 addElementListener(document.getElementsByTagName('div'));
