f2f5c8097d0bad93540da40c966a1072e0d868b8
batemanvo <batemanvo@gmail.com>
2020-05-23 17:33:12 -0700

    Adding comments to method headers and making a few slight optimizations.

--- js/hard-mode.js
+++ js/hard-mode.js
@@ -1,7 +1,12 @@
+/**
+* Class that can be optionally enabled to override the choice of which element to choose in the tic-tac-toe.js file's
+* computerMove method. Written using only pre-ES6 features to help junior developers learning JavaScript have an
+* easier time reading.
+*/
 var HardMode = {};
 (function() {
     var EMPTY_SPACE_CLASS = 'unmarked';
-    var WINNING_COMBINATIONS = HardMode.WINNING_COMBINATIONS = [
+    var WINNING_COMBINATIONS = [
         ['a-box', 'b-box', 'c-box'],
         ['d-box', 'e-box', 'f-box'],
         ['g-box', 'h-box', 'i-box'],
@@ -12,6 +17,8 @@ var HardMode = {};
         ['c-box', 'e-box', 'g-box']
    ];
 
+   HardMode.isEnabled = false;
+   
     /**
     * Gets all empty spaces on the board and checks if any of them are the last space needed to make a winning move.
     * If so, returns the winning space's element Id. Otherwise, returns undefined.
@@ -54,10 +61,13 @@ var HardMode = {};
         }
     };
 
-    function buildSelectors(id) {
-        return '#' + id + '.' + EMPTY_SPACE_CLASS;
-    }
-
+    /**
+    * Determines an element for the computer to place a marker in, according to the following priority:
+    * 1. Winning move (either for the computer or to block the player)
+    * 2. Center space
+    * 3. Corner space
+    * 4. Any remaining available space
+    */
     HardMode.computerMove = function() {
         var winningId = HardMode.getWinningId();
         if (winningId) {
@@ -69,14 +79,15 @@ var HardMode = {};
             return centerSpace;
         }
 
-        var emptyCornerSelectors = ['a-box', 'c-box', 'g-box', 'i-box'].map(buildSelectors);
+        var emptyCornerSelectors = ['a-box', 'c-box', 'g-box', 'i-box'].map(function(id) {
+            return '#' + id + '.' + EMPTY_SPACE_CLASS;
+        });
         var emptyCorner = document.querySelector(emptyCornerSelectors.toString());
         if (emptyCorner) {
             return emptyCorner;
         }
 
-        var emptySideSelectors = ['b-box', 'd-box', 'f-box', 'h-box'].map(buildSelectors);
-        return document.querySelector(emptySideSelectors);
+        return document.querySelector('.' + EMPTY_SPACE_CLASS);
     };
 
     module.exports = HardMode;
